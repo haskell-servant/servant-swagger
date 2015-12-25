9ddf11755d96a07f66b80a19bb72cabcf05e2940
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2015-12-25 13:22:01 +0300

    Add Hackage API test example with sub API tags

--- test/Servant/SwaggerSpec.hs
+++ test/Servant/SwaggerSpec.hs
@@ -1,15 +1,18 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE QuasiQuotes #-}
 module Servant.SwaggerSpec where
 
+import Control.Lens
 import Data.Aeson
 import Data.Aeson.QQ
 import Data.Char (toLower)
 import Data.Proxy
-import Data.Swagger hiding (Tag(..))
+import Data.Swagger
+import Data.Text (Text)
 import Data.Time
 import GHC.Generics
 import Servant.API
@@ -17,11 +20,15 @@ import Servant.Swagger
 import Test.Hspec
 
 checkAPI :: HasSwagger api => Proxy api -> Value -> IO ()
-checkAPI proxy spec = toJSON (toSwagger proxy) `shouldBe` spec
+checkAPI proxy = checkSwagger (toSwagger proxy)
+
+checkSwagger :: Swagger -> Value -> IO ()
+checkSwagger spec js = toJSON spec `shouldBe` js
 
 spec :: Spec
-spec = describe "HasSwagger" $
+spec = describe "HasSwagger" $ do
   it "Todo API" $ checkAPI (Proxy :: Proxy TodoAPI) todoAPI
+  it "Hackage API (with tags)" $ checkSwagger hackageSwaggerWithTags hackageAPI
 
 main :: IO ()
 main = hspec spec
@@ -96,6 +103,196 @@ todoAPI = [aesonQQ|
             }
         }
     }
+}
+|]
+
+-- =======================================================================
+-- Hackage API
+-- =======================================================================
+
+type HackageAPI
+    = HackageUserAPI
+ :<|> HackagePackagesAPI
+
+type HackageUserAPI =
+      "users" :> Get '[JSON] [UserSummary]
+ :<|> "user" :> Capture "username" Username :> Get '[JSON] UserDetailed
+
+type HackagePackagesAPI
+    = "packages" :> Get '[JSON] [Package]
+
+type Username = Text
+
+data UserSummary = UserSummary
+  { summaryUsername :: Username
+  , summaryUserid   :: Int
+  } deriving (Eq, Show, Generic, ToSchema)
+
+type Group = Text
+
+data UserDetailed = UserDetailed
+  { username :: Username
+  , userid   :: Int
+  , groups   :: [Group]
+  } deriving (Eq, Show, Generic, ToSchema)
+
+newtype Package = Package { packageName :: Text }
+  deriving (Eq, Show, Generic, ToSchema)
+
+hackageSwaggerWithTags :: Swagger
+hackageSwaggerWithTags = toSwagger (Proxy :: Proxy HackageAPI)
+  & addTag "users"    (Proxy :: Proxy HackageUserAPI)     (Proxy :: Proxy HackageAPI)
+  & addTag "packages" (Proxy :: Proxy HackagePackagesAPI) (Proxy :: Proxy HackageAPI)
+  & tags .~
+      [ Tag "users" (Just "Operations about user") Nothing
+      , Tag "packages" (Just "Query packages") Nothing
+      ]
+
+hackageAPI :: Value
+hackageAPI = [aesonQQ|
+{
+   "swagger":"2.0",
+   "info":{
+      "version":"",
+      "title":""
+   },
+   "definitions":{
+      "UserDetailed":{
+         "required":[
+            "username",
+            "userid",
+            "groups"
+         ],
+         "type":"object",
+         "properties":{
+            "groups":{
+               "items":{
+                  "type":"string"
+               },
+               "type":"array"
+            },
+            "username":{
+               "type":"string"
+            },
+            "userid":{
+               "maximum":9223372036854775807,
+               "minimum":-9223372036854775808,
+               "type":"integer"
+            }
+         }
+      },
+      "Package":{
+         "required":[
+            "packageName"
+         ],
+         "type":"object",
+         "properties":{
+            "packageName":{
+               "type":"string"
+            }
+         }
+      },
+      "UserSummary":{
+         "required":[
+            "summaryUsername",
+            "summaryUserid"
+         ],
+         "type":"object",
+         "properties":{
+            "summaryUsername":{
+               "type":"string"
+            },
+            "summaryUserid":{
+               "maximum":9223372036854775807,
+               "minimum":-9223372036854775808,
+               "type":"integer"
+            }
+         }
+      }
+   },
+   "paths":{
+      "/users":{
+         "get":{
+            "responses":{
+               "200":{
+                  "schema":{
+                     "items":{
+                        "$ref":"#/definitions/UserSummary"
+                     },
+                     "type":"array"
+                  },
+                  "description":""
+               }
+            },
+            "produces":[
+               "application/json"
+            ],
+            "tags":[
+               "users"
+            ]
+         }
+      },
+      "/packages":{
+         "get":{
+            "responses":{
+               "200":{
+                  "schema":{
+                     "items":{
+                        "$ref":"#/definitions/Package"
+                     },
+                     "type":"array"
+                  },
+                  "description":""
+               }
+            },
+            "produces":[
+               "application/json"
+            ],
+            "tags":[
+               "packages"
+            ]
+         }
+      },
+      "/user/{username}":{
+         "get":{
+            "responses":{
+               "404":{
+                  "description":"username not found"
+               },
+               "200":{
+                  "schema":{
+                     "$ref":"#/definitions/UserDetailed"
+                  },
+                  "description":""
+               }
+            },
+            "produces":[
+               "application/json"
+            ],
+            "parameters":[
+               {
+                  "required":true,
+                  "in":"path",
+                  "name":"username",
+                  "type":"string"
+               }
+            ],
+            "tags":[
+               "users"
+            ]
+         }
+      }
+   },
+   "tags":[
+      {
+         "name":"users",
+         "description":"Operations about user"
+      },
+      {
+         "name":"packages",
+         "description":"Query packages"
+      }
+   ]
 }
 |]
 

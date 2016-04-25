ce5e05fde2cce04aa11842b23e65930805d26be2
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2016-04-25 20:33:38 +0300

    Turn off doctests for servant-0.4

--- test/DocTest.hs
+++ test/DocTest.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE CPP #-}
 module Main (main) where
 
 import System.FilePath.Glob (glob)
 import Test.DocTest (doctest)
 
 main :: IO ()
-main = glob "src/**/*.hs" >>= doctest
-
+main = do
+#if MIN_VERSION_servant(0,5,0)
+  glob "src/**/*.hs" >>= doctest
+#else
+  return ()
+#endif
